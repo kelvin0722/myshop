d5bb7979ac865ce92e094431ffc460d39ec6e557
kelvin0722 <kelvinm442@gmail.com>
2024-03-12 12:20:08 +0300

    :art: Project setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'dev-images-carry1st-products.s3.eu-west-2.amazonaws.com'
+            },
+        ],
+    },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/products',
+                permanent: true,
+            },
+        ]
+    },
+};
 
 export default nextConfig;
